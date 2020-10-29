29233d9dfab6db2c2d6744b700dff70ac63e946a
Okimura Itsuki <sevan0808@gmail.com>
2020-10-29 15:01:48 +0900

    fix

--- sketch_oct23a.ino
+++ sketch_oct23a.ino
@@ -1,22 +1,22 @@
 #include "HUSKYLENS.h"
 #include "SoftwareSerial.h"
 
-HUSKYLENS huskylens:
-SoftwareSerial mySerial(5, 23)
+HUSKYLENS huskylens;
+SoftwareSerial mySerial(4, 0);
+//HUSKYLENS green line >> Pin 10; blue line >>Pin11
 void printResult(HUSKYLENSResult result);
 
 void setup() {
-  pinMode(2, OUTPUT)
-  Serial.begin(115200);
-  mySerial.begin(9600);
-  while (!huskylens.begin(mySerial))
-  {
-      Serial.println(F("Begin failed!"));
-      Serial.println(F("1.Please retcheck the \"Protocol Type\" in HUSKYLENS (General Setting>>Protocol Type>>Serrial 9600)"));
-      Serial.println(F("2.Please retcheck the connection."));
-      delay(100);
-  }
-
+    pinMode(2, OUTPUT);
+    Serial.begin(115200);
+    mySerial.begin(9600);
+    while (!huskylens.begin(mySerial))
+    {
+        Serial.println(F("Begin failed!"));
+        Serial.println(F("1.Please retcheck the \"Protocol Type\" in HUSKYLENS (General Setting>>Protocol Type>>Serrial 9600)"));
+        Serial.println(F("2.Please retcheck the connection."));
+        delay(100);
+    }
 }
 
 void loop() {
@@ -28,17 +28,21 @@ void loop() {
       Serial.println(F("Nothing learned, press learn button on HUSKYLENS to leran one!"));
       delay(500);
     }
+    else if(!huskylens.available()){
+      Serial.println(F("No block or arrow apears on the screen!"));
+      delay(500);
+    }
     else
     {
         while (huskylens.available())
         {
-            HUSKYLENSResult = huskylens.read();
+            HUSKYLENSResult result = huskylens.read();
             printResult(result);
         }
     }
 }
 
-void printResult(HuskylensResult result){
+void printResult(HUSKYLENSResult result){
     if (result.command == COMMAND_RETURN_BLOCK){
         if (result.xCenter > 160){
             Serial.println(F("Obeject's Position : RIGHT"));
@@ -56,7 +60,7 @@ void printResult(HuskylensResult result){
         }
     }
     else if (result.command == COMMAND_RETURN_ARROW){
-        Serial.println(String()+F("Arrow:xOrigin=")+result.xOrigin+F(",yOrigin=")+result.yOrigin+F(",xTarget=")+reult.xTarget+F(",yTarget=")+result.yTarget+F(",ID=")+result.ID);
+        Serial.println(String()+F("Arrow:xOrigin=")+result.xOrigin+F(",yOrigin=")+result.yOrigin+F(",xTarget=")+result.xTarget+F(",yTarget=")+result.yTarget+F(",ID=")+result.ID);
         delay(500);
     }
     else{
